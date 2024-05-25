d0f48b023e3fb67f2be52b40ee0a5ed8af39ca41
abdullah <abdullahiqbal599@gmail.com>
2024-05-25 10:25:22 +0500

    create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { FaSearch, FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header className="bg-gray-800 text-white p-4 ">
+      <div className="flex items-center justify-between max-w-6xl mx-auto">
+        <Link to="/">
+          <h1 className="text-lg font-bold sm:text-xl flex flex-wrap">
+            <span>Local</span>
+            <span className="text-blue-400">Estate</span>
+          </h1>
+        </Link>
+        <div className="flex justify-between ">
+          <div className="relative w-full sm:w-auto">
+            <form>
+              <input
+                type="text"
+                placeholder="Search..."
+                className="w-28 sm:w-96 pl-10 pr-4 py-2 rounded-full bg-gray-700 text-white placeholder-gray-400 focus:outline-none"
+              />
+              <FaSearch className="block absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-500" />
+            </form>
+          </div>
+        </div>
+
+        {/* <ul className={`sm:flex space-x-4 hidden`}>
+          <li className="hover:underline cursor-pointer">Home</li>
+          <li className="hover:underline cursor-pointer">About</li>
+          <li className="hover:underline cursor-pointer">Sign In</li>
+        </ul> */}
+        {/* {menuOpen && (
+        <ul className="flex flex-col mt-4 space-y-2 sm:hidden">
+          <li className="hover:underline cursor-pointer">Home</li>
+          <li className="hover:underline cursor-pointer">About</li>
+          <li className="hover:underline cursor-pointer">Sign In</li>
+        </ul>
+      )} */}
+
+        <div className="sm:hidden">
+          {menuOpen ? (
+            <FaTimes className="text-xl cursor-pointer" onClick={toggleMenu} />
+          ) : (
+            <FaBars className="text-xl cursor-pointer" onClick={toggleMenu} />
+          )}
+        </div>
+
+        <div className="">
+          <ul
+            className={`flex-col space-y-2 ${
+              menuOpen ? "flex" : "hidden"
+            } sm:flex sm:flex-row sm:space-y-0 sm:space-x-4 `}
+          >
+            <Link to="/">
+              <li className="hover:underline cursor-pointer">Home</li>
+            </Link>
+            <Link to="/about">
+              <li className="hover:underline cursor-pointer">About</li>
+            </Link>
+            <Link to="/sign-in">
+              <li className="hover:underline cursor-pointer">Sign In</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+}
